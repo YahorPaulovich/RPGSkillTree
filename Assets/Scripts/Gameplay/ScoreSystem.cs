8244ae75b52b2e122680ce48f5da35eb4f1134dc
Egor Pavlovich <yahorpaulovich@gmail.com>
2023-05-14 22:24:24 +0200

    Score Indicator Bar has been implemented
    Briefly:
    1) Added two scripts: ScoreSystem.cs and ScoreBarFade.cs
    2) Added two png files: "Score Indicator Background Rectangle.png" and "Score Indicator Filler Rectangle.png"

---
+++ Assets/Scripts/Gameplay/ScoreSystem.cs
@@ -0,0 +1,41 @@
+using System;
+
+public class ScoreSystem
+{
+    public event EventHandler OnSpent;
+    public event EventHandler OnEarned;
+
+    public int ScoreAmount;
+    private int _scoreAmountMax;
+
+    public ScoreSystem(int scoreAmount)
+    {
+        _scoreAmountMax = scoreAmount;
+        ScoreAmount = scoreAmount;
+    }
+
+    public void Spend(int amount)
+    {
+        ScoreAmount -= amount;
+        if (ScoreAmount < 0)
+        {
+            ScoreAmount = 0;
+        }
+        if (OnSpent != null) OnSpent(this, EventArgs.Empty);
+    }
+
+    public void Earn(int amount)
+    {
+        ScoreAmount += amount;
+        if (ScoreAmount > _scoreAmountMax)
+        {
+            ScoreAmount = _scoreAmountMax;
+        }
+        if (OnEarned != null) OnEarned(this, EventArgs.Empty);
+    }
+
+    public float GetScoreNormalized()
+    {
+        return (float)ScoreAmount / _scoreAmountMax;
+    }
+}
---
+++ Assets/Scripts/UI/ScoreBarFade.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreBarFade : MonoBehaviour
+{
+    private const float SPENT_SCORE_FADE_TIMER_MAX = 0.6f;
+
+    [SerializeField] private Image _barImage;
+    [SerializeField] private Image _spentBarImage;
+    [SerializeField] private TMP_Text _scoreText;
+    private Color _spentColor;
+    private float _spentScoreFadeTimer;
+    private ScoreSystem _scoreSystem;
+
+    private void Awake()
+    {
+        _spentColor = _spentBarImage.color;
+        _spentColor.a = 0f;
+        _spentBarImage.color = _spentColor;
+    }
+
+    private void Start()
+    {
+        _scoreSystem = new ScoreSystem(100);
+        SetScore(_scoreSystem.GetScoreNormalized());
+        _scoreSystem.OnSpent += ScoreSystem_OnSpent;
+        _scoreSystem.OnEarned += ScoreSystem_OnEarned;
+    }
+
+    private void Update()
+    {
+        if (_spentColor.a > 0)
+        {
+            _spentScoreFadeTimer -= Time.deltaTime;
+            if (_spentScoreFadeTimer < 0)
+            {
+                float fadeAmount = 5f;
+                _spentColor.a -= fadeAmount * Time.deltaTime;
+                _spentBarImage.color = _spentColor;
+            }
+        }
+    }
+
+    public void EarnPoints()
+    {
+        _scoreSystem.Earn(10);       
+        _scoreText.text = _scoreSystem.ScoreAmount.ToString();
+    }
+
+    public void SpendPoints()
+    {
+        _scoreSystem.Spend(10);
+        _scoreText.text = _scoreSystem.ScoreAmount.ToString();
+    }
+
+    public void EarnAllPoints()
+    {
+        _scoreSystem.Earn(100);
+        _scoreText.text = _scoreSystem.ScoreAmount.ToString();
+    }
+
+    private void ScoreSystem_OnEarned(object sender, System.EventArgs e)
+    {
+        SetScore(_scoreSystem.GetScoreNormalized());
+    }
+
+    private void ScoreSystem_OnSpent(object sender, System.EventArgs e)
+    {
+        if (_spentColor.a <= 0)
+        {
+            // Spent bar image is invisible
+            _spentBarImage.fillAmount = _barImage.fillAmount;
+        }
+        _spentColor.a = 1;
+        _spentBarImage.color = _spentColor;
+        _spentScoreFadeTimer = SPENT_SCORE_FADE_TIMER_MAX;
+
+        SetScore(_scoreSystem.GetScoreNormalized());
+    }
+
+    private void SetScore(float healthNormalized)
+    {
+        _barImage.fillAmount = healthNormalized;
+    }
+}
